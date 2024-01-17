fe8ea9eab9f197bc403f1f463e41d5bd130ed886
Layoute <qingfengroot@163.com>
2024-01-17 16:59:56 +0800

    feat: optimize status bar with lualine

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -57,6 +57,7 @@ return {
 		"rcarriga/nvim-notify",
 		opts = {
 			timeout = 5000,
+			background_colour = '#00000000',
 		},
 	},
 
@@ -89,51 +90,124 @@ return {
 		},
 	},
 
-	-- statusline
 	{
 		"nvim-lualine/lualine.nvim",
 		event = "VeryLazy",
-		opts = {
-			options = {
-				-- globalstatus = false,
-				icons_enabled = true,
-				theme = 'auto',
-				component_separators = { left = '', right = ''},
-				section_separators = { left = '', right = ''},
-				disabled_filetypes = {
-					statusline = {},
-					winbar = {},
+		init = function()
+			vim.g.lualine_laststatus = vim.o.laststatus
+			if vim.fn.argc(-1) > 0 then
+				-- set an empty statusline till lualine loads
+				vim.o.statusline = " "
+			else
+				-- hide the statusline on the starter page
+				vim.o.laststatus = 0
+			end
+		end,
+		opts = function()
+			local Util = require("lazyvim.util")
+			-- PERF: we don't need this lualine require madness 🤷
+			local lualine_require = require("lualine_require")
+			lualine_require.require = require
+
+			local icons = require("lazyvim.config").icons
+
+			vim.o.laststatus = vim.g.lualine_laststatus
+
+			return {
+				options = {
+					icons_enabled = true,
+					theme = 'auto',
+					component_separators = { left = '', right = ''},
+					section_separators = { left = '', right = ''},
+					disabled_filetypes = {
+						statusline = { "dashboard", "alpha", "starter" },
+						winbar = {},
+					},
+					ignore_focus = {},
+					always_divide_middle = true,
+					globalstatus = false,
+					refresh = {
+						statusline = 1000,
+						tabline = 1000,
+						winbar = 1000,
+					}
 				},
-				ignore_focus = {},
-				always_divide_middle = true,
-				globalstatus = false,
-				refresh = {
-					statusline = 1000,
-					tabline = 1000,
-					winbar = 1000,
-				}
-			},
-			sections = {
-				lualine_a = {'mode'},
-				lualine_b = {'branch', 'diff', 'diagnostics'},
-				lualine_c = {'filename'},
-				lualine_x = {'encoding', 'fileformat', 'filetype'},
-				lualine_y = {'progress'},
-				lualine_z = {'location'}
-			},
-			inactive_sections = {
-				lualine_a = {},
-				lualine_b = {},
-				lualine_c = {'filename'},
-				lualine_x = {'location'},
-				lualine_y = {},
-				lualine_z = {}
-			},
-			tabline = {},
-			winbar = {},
-			inactive_winbar = {},
-			extensions = {}
-		},
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "branch" },
+
+					lualine_c = {
+						Util.lualine.root_dir(),
+						{
+							"diagnostics",
+							symbols = {
+								error = icons.diagnostics.Error,
+								warn = icons.diagnostics.Warn,
+								info = icons.diagnostics.Info,
+								hint = icons.diagnostics.Hint,
+							},
+						},
+						{ "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+						{ Util.lualine.pretty_path() },
+					},
+					lualine_x = {
+						-- stylua: ignore
+						{
+							function() return require("noice").api.status.command.get() end,
+							cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+							color = Util.ui.fg("Statement"),
+						},
+						-- stylua: ignore
+						{
+							function() return require("noice").api.status.mode.get() end,
+							cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+							color = Util.ui.fg("Constant"),
+						},
+						-- stylua: ignore
+						{
+							function() return "  " .. require("dap").status() end,
+							cond = function () return package.loaded["dap"] and require("dap").status() ~= "" end,
+							color = Util.ui.fg("Debug"),
+						},
+						{
+							require("lazy.status").updates,
+							cond = require("lazy.status").has_updates,
+							color = Util.ui.fg("Special"),
+						},
+						{
+							"diff",
+							symbols = {
+								added = icons.git.added,
+								modified = icons.git.modified,
+								removed = icons.git.removed,
+							},
+							source = function()
+								local gitsigns = vim.b.gitsigns_status_dict
+								if gitsigns then
+									return {
+										added = gitsigns.added,
+										modified = gitsigns.changed,
+										removed = gitsigns.removed,
+									}
+								end
+							end,
+						},
+					},
+					lualine_y = {
+						{ "encoding", separator = "|", padding = { left = 1, right = 1 } },
+						{ "fileformat", separator = "|", padding = { left = 1, right = 1 } },
+						{ "progress", separator = "", padding = { left = 1, right = 1 } },
+						{ "location", padding = { left = 0, right = 1 } },
+					},
+					lualine_z = {
+						function()
+							return " " .. os.date("%R")
+						end,
+					},
+				},
+				extensions = { "neo-tree", "lazy" },
+			}
+		end,
 	},
 
 	-- filename
