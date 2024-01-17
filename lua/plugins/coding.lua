9c37ba5bff11c5324fec7aebac038f638ca3ecab
Layoute <qingfengroot@163.com>
2024-01-17 17:10:58 +0800

    feat: enable rainbow brackets for coding

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -115,5 +115,37 @@ return {
 		opts = {
 			enable_autocmd = false,
 		},
-	}
+	},
+
+	-- rainbow brackets
+	{
+		"HiPhish/rainbow-delimiters.nvim",
+		config = function ()
+			local rainbow_delimiters = require 'rainbow-delimiters'
+			-- This module contains a number of default definitions
+			require('rainbow-delimiters.setup').setup {
+				strategy = {
+					[''] = rainbow_delimiters.strategy['global'],
+					vim = rainbow_delimiters.strategy['local'],
+				},
+				query = {
+						[''] = 'rainbow-delimiters',
+						lua = 'rainbow-blocks',
+				},
+				priority = {
+						[''] = 110,
+						lua = 210,
+				},
+				highlight = {
+						'RainbowDelimiterRed',
+						'RainbowDelimiterYellow',
+						'RainbowDelimiterBlue',
+						'RainbowDelimiterOrange',
+						'RainbowDelimiterGreen',
+						'RainbowDelimiterViolet',
+						'RainbowDelimiterCyan',
+				},
+			}
+		end,
+	},
 }
